f7a7ed905e4f30ef6aba3a5a83ee18276850ddc2
Cruelasss <irula5198@gmail.com>
2025-11-20 18:02:28 +0700

    setup Column layout dengan padding isiRuang dan arrangement SpaceBetween

--- app/src/main/java/com/example/mvvm/view/uicontroller/TampilSiswa.kt
+++ app/src/main/java/com/example/mvvm/view/uicontroller/TampilSiswa.kt
@@ -1,10 +1,19 @@
 package com.example.mvvm.view.uicontroller
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import com.example.mvvm.R
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
@@ -30,3 +39,9 @@ fun TampilSiswa(
             )
         }
     )
+    { isiRuang ->
+
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
